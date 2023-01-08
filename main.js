8428a021e0b3dec05fbf7fb7f963c6c3d45aaf9e
PrimeFord <bashfordbash@gmail.com>
2023-01-08 11:45:37 +0100

    Portfolio

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 //Selector
 const cursor = document.querySelector(".cursor");
 const navbar = document.querySelector(".navbar");
+const a = document.querySelectorAll("#a");
+
+a.style.color = "#b4b4b4";
 
 //Event-listener
 document.addEventListener("mousemove", (e) => {
@@ -22,4 +25,23 @@ document.addEventListener("dblclick", () => {
   }, 200);
 });
 
-navbar;
+document.addEventListener("scroll", () => {
+  if (window.top.scrollY > 19) {
+    navbar.classList.add("scroll");
+  } else {
+    navbar.classList.remove("scroll");
+    navbar.style.transition = ".4s ease";
+  }
+});
+
+function changeBg() {
+  var nav = document.querySelector("#navbar");
+  var scrollValue = window.scrollY;
+  if (scrollValue < 635) {
+    nav.classList.remove("navcolor");
+  } else {
+    nav.classList.add("navcolor");
+  }
+}
+
+window.addEventListener("scroll", changeBg);
